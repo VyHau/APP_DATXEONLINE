5d526a449c7629a3ff4dfad6ff8a6bd3cbf8430a
Lê Hoàng Quách Tỉnh <23115053122145@sv.ute.udn.vn>
2025-03-23 18:25:39 +0700

    chỉnh sửa

---
+++ APP_DATXE/src/main/java/me/myproject/Utilities/ImageAccess.java
@@ -0,0 +1,19 @@
+package me.myproject.Utilities;
+
+import java.net.URL;
+
+public  class ImageAccess {
+    private static final String pathImage="../IMAGE/";
+    private static final String pathIcon="../IMAGE/ICON/";
+
+    public static final URL image1 = ImageAccess.class.getResource(pathImage + "co.jpg");
+    public static final String image2 =pathImage + "co.jpg";
+    public static final URL iconError = ImageAccess.class.getResource(pathIcon + "error.png");
+    public static final String image3 = pathIcon + "error.png";
+
+
+
+    
+
+}
+
