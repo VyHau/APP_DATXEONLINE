c24a96a0864a979759f7fb2544ce92ad77bb6c18
VyHau <vyhau2005@gmail.com>
2025-04-19 16:36:01 +0700

    Cap nhat mau ColorMain.java

--- src/main/java/me/myproject/Utilities/ColorMain.java
+++ src/main/java/me/myproject/Utilities/ColorMain.java
@@ -1,10 +1,12 @@
 package me.myproject.Utilities;
 import java.awt.Color;
 public  class ColorMain {
-        public static Color colorPrimary=new Color(137,217 ,202 );
-        public static Color colorSecondary=new Color(247,239 ,218 );
+        public static Color colorPrimary=new Color(23, 107, 230);
+        public static Color colorSecondary=new Color(19, 166, 221);
         public static Color colorThird=new Color(248,218 ,233 );
         public static Color colorRed = Color.RED;
+        // public static Color colorPrimary=new Color(137,217 ,202 );
+        // public static Color colorSecondary=new Color(247,239 ,218 );
         // public Color colorRed = Color.RED;
         // public Color colorRed = Color.RED;
         // public Color colorRed = Color.RED;
