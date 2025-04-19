6c0befda45b45667159e3b8d72c696cd2d2f92fc
VyHau <vyhau2005@gmail.com>
2025-04-19 21:59:32 +0700

    Them mau ColorMain.java

--- src/main/java/me/myproject/Utilities/ColorMain.java
+++ src/main/java/me/myproject/Utilities/ColorMain.java
@@ -5,9 +5,10 @@ public  class ColorMain {
         public static Color colorSecondary=new Color(19, 166, 221);
         public static Color colorThird=new Color(248,218 ,233 );
         public static Color colorRed = Color.RED;
+        public static Color colorBlueBlack1 = new Color(41, 128, 185);//xanh đậm đen
+        public static Color colorBlueBlack2 = new Color(52, 152, 219);
         // public static Color colorPrimary=new Color(137,217 ,202 );
         // public static Color colorSecondary=new Color(247,239 ,218 );
-        // public Color colorRed = Color.RED;
         // public Color colorRed = Color.RED;
         // public Color colorRed = Color.RED;
         // public Color colorRed = Color.RED;
