fb402d22a0bfde694f4b6ff66977ab6ea7d99046
VyHau <vyhau2005@gmail.com>
2025-04-19 16:13:51 +0700

    Sua email -> dia chi DangKyBSL.java

--- src/main/java/me/myproject/BUSINESSLOGIC/DangKyBSL.java
+++ src/main/java/me/myproject/BUSINESSLOGIC/DangKyBSL.java
@@ -10,7 +10,6 @@ public class DangKyBSL {
     public String xuLyDangKy(String fullName, String phone, String email, String password) {
         if (fullName == null || fullName.trim().isEmpty()) return "Vui lòng nhập họ tên.";
         if (!SDTHopLe(phone)) return "Số điện thoại không hợp lệ. Phải bắt đầu bằng 03-09 và có 10 chữ số.";
-        if (!EmailHopLe(email)) return "Email không hợp lệ.";
         if (!matKhauHopLe(password)) return "Mật khẩu phải có ít nhất 5 ký tự, gồm chữ hoa, chữ thường, số và ký tự đặc biệt.";
         return "Đăng ký thành công!";
     }
@@ -32,13 +31,12 @@ public class DangKyBSL {
         Matcher matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
-
-    public boolean EmailHopLe(String email) {
-        final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-        Pattern pattern = Pattern.compile(EMAIL_REGEX);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
+    // public boolean EmailHopLe(String email) {
+    //     final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+    //     Pattern pattern = Pattern.compile(EMAIL_REGEX);
+    //     Matcher matcher = pattern.matcher(email);
+    //     return matcher.matches();
+    // }
 
     public boolean matKhauHopLe(String password) {
         return password.length() >= 5 &&
