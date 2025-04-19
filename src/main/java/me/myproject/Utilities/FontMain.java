1595b72c08d341154df893cc5b04e540af6d323a
VyHau <vyhau2005@gmail.com>
2025-04-19 16:06:41 +0700

    Tao FontMain.java

---
+++ src/main/java/me/myproject/Utilities/FontMain.java
@@ -0,0 +1,9 @@
+package me.myproject.Utilities;
+
+import java.awt.Font;
+
+public class FontMain {
+    public static Font TITLE_FONT = new Font("Arial", Font.BOLD, 28);
+    public static Font LABEL_FONT = new Font("Arial", Font.BOLD, 14);
+    public static Font TEXT_FONT = new Font("Arial", Font.PLAIN, 14);
+}
