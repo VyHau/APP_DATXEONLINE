8dcfa50ebe5f727a212e7d5f212fa63a0c131c7a
VyHau <vyhau2005@gmail.com>
2025-04-24 16:23:26 +0700

    Add new color

--- src/main/java/me/myproject/Utilities/ColorMain.java
+++ src/main/java/me/myproject/Utilities/ColorMain.java
@@ -1,12 +1,16 @@
 package me.myproject.Utilities;
+
 import java.awt.Color;
-public  class ColorMain {
-        public static Color colorPrimary=new Color(23, 107, 230);
-        public static Color colorSecondary=new Color(19, 166, 221);
-        public static Color colorThird=new Color(248,218 ,233 );
+
+public class ColorMain {
+        public static Color colorPrimary = new Color(23, 107, 230);
+        public static Color colorSecondary = new Color(19, 166, 221);
+        public static Color colorThird = new Color(135, 206, 250);
+        public static Color inforPanel = new Color(240, 240, 240);
         public static Color colorRed = Color.RED;
-        public static Color colorBlueBlack1 = new Color(41, 128, 185);//xanh đậm đen
+        public static Color colorBlueBlack1 = new Color(41, 128, 185);// xanh đậm đen
         public static Color colorBlueBlack2 = new Color(52, 152, 219);
+        public static Color colorBlue1 = new Color(255, 255, 255, 245);
         // public static Color colorPrimary=new Color(137,217 ,202 );
         // public static Color colorSecondary=new Color(247,239 ,218 );
         // public Color colorRed = Color.RED;
