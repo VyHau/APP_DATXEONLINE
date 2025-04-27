508ebc732b8729c51bc0171ceb854ffe9cecbbde
duyphuong0103 <nguyenthiduyphuong0103@gmail.com>
2025-04-27 16:14:16 +0700

    Update ColorMain.java

--- src/main/java/me/myproject/Utilities/ColorMain.java
+++ src/main/java/me/myproject/Utilities/ColorMain.java
@@ -11,6 +11,9 @@ public class ColorMain {
         public static Color colorBlueBlack1 = new Color(41, 128, 185);// xanh đậm đen
         public static Color colorBlueBlack2 = new Color(52, 152, 219);
         public static Color colorBlue1 = new Color(255, 255, 255, 245);
+        public static Color blueHeader = new Color(20, 180, 180);//header của các giao diện
+        public static Color btnHeader = new Color(0, 178, 192);
+        public static Color blueMenuLeft = new Color(25, 37, 60); //Xanh đậm của menu trái
         // public static Color colorPrimary=new Color(137,217 ,202 );
         // public static Color colorSecondary=new Color(247,239 ,218 );
         // public Color colorRed = Color.RED;
